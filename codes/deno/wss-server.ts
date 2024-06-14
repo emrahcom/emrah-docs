3dbf7f780d0444e9be5aed4b759aec77bb017b3d
emrah <emrah.eryilmaz@nordeck.net>
2024-06-14 12:22:51 +0300

    wss-server: updated

--- codes/deno/wss-server.ts
+++ codes/deno/wss-server.ts
@@ -1,21 +1,50 @@
+// -----------------------------------------------------------------------------
+// Usage:
+//   deno run --allow-net wss-server.ts
+// -----------------------------------------------------------------------------
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const HOSTNAME = "0.0.0.0";
 const PORT = 8000;
 
 // -----------------------------------------------------------------------------
-function onMessage(e: MessageEvent) {
+function onOpen(e: Event) {
+  console.log("onOpen");
+  console.log(e);
+}
+
+// -----------------------------------------------------------------------------
+function onMessage(ws: WebSocket, e: MessageEvent) {
+  console.log("onMessage");
+  console.log(ws);
   console.log(e.data);
+  ws.send("ok");
+}
+
+// -----------------------------------------------------------------------------
+function onClose(e: CloseEvent) {
+  console.log("onClose");
+  console.log(e);
+}
+
+// -----------------------------------------------------------------------------
+function onError(e: Event | ErrorEvent) {
+  console.log("onError");
+  console.log(e);
 }
 
 // -----------------------------------------------------------------------------
 function handler(req: Request): Response {
   if (req.headers.get("upgrade") != "websocket") {
+    console.log("no upgrade request, rejected");
     return new Response(null, { status: 501 });
   }
 
   const { socket, response } = Deno.upgradeWebSocket(req);
-  socket.onmessage = onMessage;
+  socket.onopen = onOpen;
+  socket.onmessage = (e) => onMessage(socket, e);
+  socket.onclose = onClose;
+  socket.onerror = onError;
 
   return response;
 }
